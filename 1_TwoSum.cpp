de2bebba265b0429990745eeca6934a6f4dfd6f2
Zh Nurlan <nurlanzhangali@gmail.com>
2020-02-16 22:21:17 +0100

    Create 1_TwoSum.cpp

---
+++ 1_TwoSum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    
+        // O(n), extra memory (n size map)
+        
+        vector<int> twoSum;
+        map<int, int> myMap;
+        
+        for (int idx = 0; idx < nums.size(); ++idx) {
+            
+            myMap.insert(pair<int, int>(nums.at(idx), idx));
+            
+            int complement = target - nums.at(idx);
+            
+            auto it = myMap.find(complement);
+            if ( it != myMap.end() && it->second != idx) {
+                
+                twoSum.push_back (idx);
+                twoSum.push_back (it->second);
+                break;
+            }
+        }
+         
+        return twoSum;
+    }
+};
