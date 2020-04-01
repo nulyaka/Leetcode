6b67546c542072dacbaeec350b34d14f8784e36e
Zh Nurlan <nurlanzhangali@gmail.com>
2020-04-02 01:32:34 +0200

    Create SingleNumber.cpp

---
+++ 30-day-challenge/week-1/SingleNumber.cpp
@@ -0,0 +1,14 @@
+// Time complexity O(n)
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            
+            nums.at(0) = nums.at(0) ^ nums.at(i);
+        }
+        
+        return nums.at(0);
+    }
+};
