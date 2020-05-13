fd5058a707b7724f28662eb806d3797a694d0a5e
Zh Nurlan <nurlanzhangali@gmail.com>
2020-05-14 00:37:07 +0200

    created

---
+++ amazon/last-stone-weight.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        priority_queue<int> maxHeap;
+        for (int &stone: stones) {
+            maxHeap.push(stone);
+        }
+        
+        while (maxHeap.size() > 1) {
+            
+            int x = maxHeap.top();
+            maxHeap.pop();
+            
+            int y = maxHeap.top();
+            maxHeap.pop();
+            
+            if (x > y) {
+                maxHeap.push(x - y);
+            }
+        }
+        
+        return maxHeap.empty() ? 0 : maxHeap.top();
+    }
+};
